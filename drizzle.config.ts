e53216174ba28da8875ae9e0073c6eeb99ebc5c5
zeroxpunk <punk.dvpn@proton.me>
2025-07-18 16:46:21 +0300

    Initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/server/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
+});
