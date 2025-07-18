5a50abde1c1d6671ba21d2f8fc1abb5717f299cc
zeroxpunk <punk.dvpn@proton.me>
2025-07-18 20:03:21 +0300

    Add reply to a tweet

--- src/server/twitter/index.ts
+++ src/server/twitter/index.ts
@@ -1,2 +1,3 @@
 export { twitterClient } from './client';
-export { fetchUserMentionsTimeline } from './mentions'; 
+export { fetchUserMentionsTimeline } from './mentions';
+export { replyToTweet } from './reply'; 
---
+++ src/server/twitter/reply.ts
@@ -0,0 +1,9 @@
+import { twitterClient } from './client';
+
+export async function replyToTweet(
+  tweetId: string,
+  text: string
+) {
+  const response = await twitterClient.v2.reply(text, tweetId);
+  return response.data;
+} 
